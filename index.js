e93d17383e9d7afff6eeb098a3c66d3e3252c545
Antonio Neto <antoniofqn@hotmail.com>
2023-04-14 00:33:11 -0300

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const sequelize = require('./config/database');
 const flashcardsRoutes = require('./routes/flashcard_routes');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(flashcardsRoutes);
 
